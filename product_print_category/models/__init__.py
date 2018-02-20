a4f8e60641aeca4032babf568dd34f1d7b7e42ac
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-02-20 14:09:18 +0100

    [ADD] migrate product_to_print

---
+++ product_print_category/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from . import product_print_category
+
+from . import product_product
+from . import product_template
+
---
+++ product_print_category/models/product_template.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    print_category_id = fields.Many2one(
+        string='Print Category', comodel_name='product.print.category')
+    to_print = fields.Boolean(
+        string='Pricetag to reprint', compute='_compute_to_print', store=True)
+
+    @api.multi
+    @api.depends('product_variant_ids.to_print')
+    def _compute_to_print(self):
+        for template in self:
+            template.to_print = any(
+                p.to_print for p in template.product_variant_ids)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('print_category_id', False):
+            vals['to_print'] = True
+        return super(ProductTemplate, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        res = super(ProductTemplate, self).write(vals)
+        template_ids = []
+        for template in self:
+            if template.print_category_id:
+                if len(list(
+                        set(vals.keys()) &
+                        set(template.print_category_id.field_ids.
+                            mapped('name')))):
+                    template_ids.append(template.id)
+        templates = self.browse(template_ids)
+        super(ProductTemplate, templates).write({'to_print': True})
+        products = self.env['product.product'].search(
+            [('product_tmpl_id', 'in', tuple(template_ids))])
+        products.write({'to_print': True})
+        return res
