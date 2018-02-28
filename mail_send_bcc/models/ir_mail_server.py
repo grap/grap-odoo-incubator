38307e37f29549e62c7b61e6d3a05adac3a9ca70
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-02-28 19:02:45 +0100

    [FIX] flake8

--- mail_send_bcc/models/ir_mail_server.py
+++ mail_send_bcc/models/ir_mail_server.py
@@ -3,7 +3,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from openerp import api, models
 from email.Utils import COMMASPACE
 
 
