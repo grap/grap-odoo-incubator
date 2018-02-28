390592d610c074ff35e886cf85f2ba72d7abcdba
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-02-28 16:20:47 +0100

    [ADD] pos_check_session_state : translation

--- pos_check_session_state/models/pos_config.py
+++ pos_check_session_state/models/pos_config.py
@@ -3,8 +3,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, fields, models
-from openerp.exceptions import Warning as UserError
+from openerp import fields, models
 
 
 class PosConfig(models.Model):
