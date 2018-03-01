9a7da5e19ccf9dda18ead0238ef1393e296a5aa7
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-03-01 14:31:25 +0100

    [REF] port product_simple_pricelist

--- product_simple_pricelist/__openerp__.py
+++ product_simple_pricelist/__openerp__.py
@@ -1,55 +1,13 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Product - Simple Pricelist module for Odoo
-#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# coding: utf-8
+# Copyright (C) 2015 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Product - Simple Pricelist',
     'summary': 'Provides Wizard to manage easily Pricelist By Products',
-    'version': '0.1',
+    'version': '8.0.1.0.0',
     'category': 'Product',
-    'description': """
-Provides Wizard to manage easily Pricelist By Products
-======================================================
-
-Functionality:
---------------
-    * A pricelist has now a new boolean field : simple_pricelist, that indicate
-      that user can use a wizard to edit the pricelist by product;
-      An "simple pricelist" has one and only one version;
-    * An "simple pricelist" has one item based on product;
-    * A wizard is available to edit the "simple pricelist", that allow users,
-      in an editable tree view, to set price by product;
-
-Roadmad / Issue
----------------
-
-create a simple pricelist should be more intuitive or more documented.
-Create a simple pricelist without creating a version will fail)
-
-Copyright, Authors and Licence:
--------------------------------
-    * Copyright: 2015, GRAP: Groupement Régional Alimentaire de Proximité;
-    * Author:
-        * Sylvain LE GAL (https://twitter.com/legalsylvain);
-    * Licence: AGPL-3 (http://www.gnu.org/licenses/);""",
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
@@ -57,10 +15,16 @@ Copyright, Authors and Licence:
         'product',
     ],
     'data': [
-        'security/ir_rule.yml',
-        'security/ir_model_access.yml',
-        'view/view.xml',
-        'view/action.xml',
+        'security/ir_rule.xml',
+        'security/ir.model.access.csv',
+        'views/view_product_pricelist.xml',
+        'views/view_product_simple_pricelist_item.xml',
+        'views/view_product_simple_pricelist_item_update_wizard.xml',
+        'views/action.xml',
+    ],
+    'demo': [
+        'demo/res_groups.xml',
+        'demo/product_pricelist.xml',
     ],
     'installable': True,
 }
