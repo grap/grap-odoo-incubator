ce8444739a4851570a3d69e83a83d2851f01382f
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-03-02 13:12:15 +0100

    [WIP] add pos_sector

---
+++ pos_sector/models/product_template.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    # Columns section
+    sector_id = fields.Many2one(
+        comodel_name='pos.sector', string='PoS Sector')
