7e484ce31f5c6afa290253a78c82d5ff50856bd1
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-03-02 13:12:15 +0100

    [WIP] add pos_sector

---
+++ pos_sector/static/src/js/pos_sector.js
@@ -0,0 +1,19 @@
+/*
+Copyright (C) 2018-Today GRAP (http://www.grap.coop)
+@author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+*/
+
+"use strict";
+
+openerp.pos_sector = function(instance){
+    var module = instance.point_of_sale;
+
+    console.log("pos_sector");
+
+};
+
+
+
+
+
