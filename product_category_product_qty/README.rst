7b6518778b6c89dd5fa4be0f363ffb7417446fd2
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-08-01 18:38:34 +0200

    [REF] product_category_product_qty

---
+++ product_category_product_qty/README.rst
@@ -0,0 +1,34 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+===================================
+Product Category - Product Quantity
+===================================
+
+This module extends the functionality of product module to display
+the quantity of products and variants for the product categories.
+
+
+.. figure:: figure:: https://raw.githubusercontent.com/grap/grap-odoo-incubator/8.0/product_category_product_qty/static/description/product_category_tree.png
+   :width: 600 px
+
+.. figure:: figure:: https://raw.githubusercontent.com/grap/grap-odoo-incubator/8.0/product_category_product_qty/static/description/product_category_form.png
+   :width: 600 px
+
+Credits
+=======
+
+Contributors
+------------
+
+* Sylvain LE GAL (https://www.twitter.com/legalsylvain)
+
+Do not contact contributors directly about support or help with technical issues.
+
+Funders
+-------
+
+The development of this module has been financially supported by:
+
+* GRAP, Groupement Régional Alimentaire de Proximité (http://www.grap.coop)
--- product_category_product_qty/__openerp__.py
+++ product_category_product_qty/__openerp__.py
@@ -1,45 +1,12 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Product - Category Improve for Odoo
-#    Copyright (C) 2014 GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# coding: utf-8
+# Copyright (C) 2014 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Product - Category Improve',
-    'version': '1.0',
+    'name': 'Product Category - Product Quantity',
+    'version': '8.0.1.0.0',
     'category': 'Product',
-    'description': """
-Improve management of Products Categories
-=========================================
-
-Functionnality:
----------------
-    * Possibility to move products to another category;
-
-TODO: (IMPROVE) Possibility to view number of product of categories;
-
-Copyright, Author and Licence:
-------------------------------
-    * Copyright: 2014, Groupement Régional Alimentaire de Proximité
-    * Author: Sylvain LE GAL (https://twitter.com/legalsylvain)
-    * Licence: AGPL-3 (http://www.gnu.org/licenses/)
-    """,
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
@@ -47,9 +14,10 @@ Copyright, Author and Licence:
         'product',
     ],
     'data': [
-        'view/view_2.xml',
-        'view/action.xml',
-        'view/view_1.xml',
+        'views/view_product_category.xml',
+    ],
+    'demo': [
+        'demo/res_groups.xml',
     ],
     'installable': True,
 }
