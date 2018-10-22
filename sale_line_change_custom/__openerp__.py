df20d44081a7875ed5366c73c28282284aa9b1f9
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-10-22 15:07:45 +0200

    [REF] move sale_line_change_custom from -misc to -incubator

---
+++ sale_line_change_custom/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-Today GRAP (http://www.grap.coop)
+# Copyright (C) 2015-Today Noemis (http://www.noemis.fr)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Sale - Custom Behaviour on Line Changes',
+    'version': '8.0.1.0.0',
+    'category': 'Sale',
+    'author': 'GRAP,Noemis',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_stock',
+    ],
+    'data': [
+        'security/res_groups.yml',
+    ],
+    'installable': True,
+}
