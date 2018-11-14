2a6b119dfd29ed6158161f4848ffd54f0bb4a43a
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-11-14 11:40:10 +0100

    [REF] move stock_easy_valuation from -misc into -incubator

---
+++ stock_easy_valuation/README.rst
@@ -0,0 +1,31 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=========================
+Add Menu to valuate stock
+=========================
+
+Pending valuation
+-----------------
+
+Add an extra menu item in 'stock' to have the valuation of all active
+product instantly
+
+Installation
+============
+
+Normal installation.
+
+Configuration
+=============
+
+No configuration is needed.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Sylvain LE GAL <https://twitter.com/legalsylvain>
---
+++ stock_easy_valuation/models/product_product.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014-Today GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models, api
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    # Compute Section
+    @api.multi
+    def _get_valuation_qty_available(self):
+        for product in self:
+            product.valuation_qty_available =\
+                product.qty_available * product.standard_price
+
+    @api.multi
+    def _get_valuation_virtual_available(self):
+        for product in self:
+            product.valuation_virtual_available =\
+                product.virtual_available * product.standard_price
+
+    # Columns Section
+    valuation_qty_available = fields.Float(
+        compute='_get_valuation_qty_available',
+        string='Valuation of Quantity on Hand')
+    valuation_virtual_available = fields.Float(
+        compute='_get_valuation_virtual_available',
+        string='Valuation of Virtual Quantity')
