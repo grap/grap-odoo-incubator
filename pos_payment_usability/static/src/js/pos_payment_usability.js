10548f9f5db18b05865ccfa21d0abad1863fed56
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-03-29 18:15:15 +0100

    wip

--- pos_payment_usability/static/src/js/pos_payment_usability.js
+++ pos_payment_usability/static/src/js/pos_payment_usability.js
@@ -4,154 +4,149 @@ Copyright (C) 2019-Today GRAP (http://www.grap.coop)
 License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
-"use strict";
+'use strict'
 
-openerp.pos_payment_usability = function(instance){
+openerp.pos_payment_usability = function (instance) {
+  var module = instance.point_of_sale
+  var _t = instance.web._t
 
-    var module = instance.point_of_sale;
-    var _t = instance.web._t;
-
-    /*************************************************************************
+  /*************************************************************************
         Extend Model Order:
     */
-    var moduleOrderParent = module.Order;
-    module.Order = module.Order.extend({
+  var moduleOrderParent = module.Order
+  module.Order = module.Order.extend({
 
-        addPaymentline: function(cashregister){
-            // Do not add payment line, if the order is empty
-            if (this.get('orderLines').length === 0 && this.get('paymentLines').length === 0) {
-                this.pos.pos_widget.screen_selector.show_popup('error', {
-                    'message': _t('Empty Order'),
-                    'comment': _t('You can not add payment on a empty order ')
-                });
-                return;
-            }
+    addPaymentline: function (cashregister) {
+      // Do not add payment line, if the order is empty
+      if (this.get('orderLines').length === 0 && this.get('paymentLines').length === 0) {
+        this.pos.pos_widget.screen_selector.show_popup('error', {
+          'message': _t('Empty Order'),
+          'comment': _t('You can not add payment on a empty order ')
+        })
+        return
+      }
 
-            // If there are an empty payment line, drop it before
-            // adding a new one
-            if (this.get('paymentLines').length > 0){
-                var paymentLines = this.get('paymentLines').models;
-                for (var i = paymentLines.length - 1; i >=0 ; i--) {
-                    var paymentLine = paymentLines[i];
+      // If there are an empty payment line, drop it before
+      // adding a new one
+      if (this.get('paymentLines').length > 0) {
+        var paymentLines = this.get('paymentLines').models
+        for (var i = paymentLines.length - 1; i >= 0; i--) {
+          var paymentLine = paymentLines[i]
 
-                    if (paymentLine.amount === 0) {
-                        this.removePaymentline(paymentLine);
-                    }
-                }
-            }
-            return moduleOrderParent.prototype.addPaymentline.apply(this, arguments);
-        },
+          if (paymentLine.amount === 0) {
+            this.removePaymentline(paymentLine)
+          }
+        }
+      }
+      return moduleOrderParent.prototype.addPaymentline.apply(this, arguments)
+    }
 
-    });
+  })
 
-    /*************************************************************************
+  /*************************************************************************
         Extend PaymentScreen Widget:
     */
-    module.PaymentScreenWidget = module.PaymentScreenWidget.extend({
+  module.PaymentScreenWidget = module.PaymentScreenWidget.extend({
 
-        validate_order: function(options) {
-            var currentOrder = this.pos.get('selectedOrder');
-            var paymentLines = currentOrder.get('paymentLines').models;
-            var paidTotal = currentOrder.getPaidTotal();
-            var dueTotal = currentOrder.getTotalTaxIncluded();
-            var remaining = dueTotal > paidTotal ? dueTotal - paidTotal : 0;
-            var change = paidTotal > dueTotal ? paidTotal - dueTotal : 0;
-            var cashAmount = 0;
+    validate_order: function (options) {
+      var currentOrder = this.pos.get('selectedOrder')
+      var paymentLines = currentOrder.get('paymentLines').models
+      var paidTotal = currentOrder.getPaidTotal()
+      var dueTotal = currentOrder.getTotalTaxIncluded()
+      var remaining = dueTotal > paidTotal ? dueTotal - paidTotal : 0
+      var change = paidTotal > dueTotal ? paidTotal - dueTotal : 0
+      var cashAmount = 0
 
-            // Delete payment lines if the amount is null
-            for (var i = paymentLines.length - 1; i >=0 ; i--) {
-                var paymentLine = paymentLines[i];
-                if (paymentLine.amount === 0) {
-                    currentOrder.removePaymentline(paymentLine);
-                }
-            }
+      // Delete payment lines if the amount is null
+      for (var i = paymentLines.length - 1; i >= 0; i--) {
+        var paymentLine = paymentLines[i]
+        if (paymentLine.amount === 0) {
+          currentOrder.removePaymentline(paymentLine)
+        }
+      }
 
-            // Check if change is over the total amount of cash received
-            if (change > 0) {
-                for (var i = 0; i < paymentLines.length; i++) {
-                    var paymentLine = paymentLines[i];
-                    if (paymentLine.get_type() === 'cash') {
-                        cashAmount += paymentLine.amount;
-                    }
-                }
-            }
-            // Display a warning if cash amount is under the change
-            if (cashAmount < change) {
-                this.pos_widget.screen_selector.show_popup('error', {
-                    'message': _t('Incorrect Change Value'),
-                    'comment': _t('The change is over the total amount of cash received')
-                });
-                return;
-            }
+      // Check if change is over the total amount of cash received
+      if (change > 0) {
+        for (var i = 0; i < paymentLines.length; i++) {
+          var paymentLine = paymentLines[i]
+          if (paymentLine.get_type() === 'cash') {
+            cashAmount += paymentLine.amount
+          }
+        }
+      }
+      // Display a warning if cash amount is under the change
+      if (cashAmount < change) {
+        this.pos_widget.screen_selector.show_popup('error', {
+          'message': _t('Incorrect Change Value'),
+          'comment': _t('The change is over the total amount of cash received')
+        })
+        return
+      }
 
-            this._super(options);
-        },
+      this._super(options)
+    },
 
-        update_payment_summary: function() {
-            var currentOrder = this.pos.get('selectedOrder');
-            // var paymentLines = currentOrder.get('paymentLines').models;
-            var paidTotal = currentOrder.getPaidTotal();
-            var dueTotal = currentOrder.getTotalTaxIncluded();
-            var remaining = dueTotal > paidTotal ? dueTotal - paidTotal : 0;
+    update_payment_summary: function () {
+      var currentOrder = this.pos.get('selectedOrder')
+      // var paymentLines = currentOrder.get('paymentLines').models;
+      var paidTotal = currentOrder.getPaidTotal()
+      var dueTotal = currentOrder.getTotalTaxIncluded()
+      var remaining = dueTotal > paidTotal ? dueTotal - paidTotal : 0
 
-            // Display or hide button to reach the amount
-            if (remaining > 0) {
-                this.$('.paymentline-set-change button').removeClass('oe_hidden');
-            } else {
-                this.$('.paymentline-set-change button').addClass('oe_hidden');
-            }
+      // Display or hide button to reach the amount
+      if (remaining > 0) {
+        this.$('.paymentline-set-change button').removeClass('oe_hidden')
+      } else {
+        this.$('.paymentline-set-change button').addClass('oe_hidden')
+      }
 
-            this._super();
-        },
+      this._super()
+    },
 
-        render_paymentline: function(line){
-            var node = this._super(line);
-            node.querySelector('.paymentline-set-change button')
-                .addEventListener('click', this.click_set_change);
-            return node;
-        },
+    render_paymentline: function (line) {
+      var node = this._super(line)
+      node.querySelector('.paymentline-set-change button')
+        .addEventListener('click', this.click_set_change)
+      return node
+    },
 
+    click_set_change: function (event) {
+      var node = this
+      while (node && !node.classList.contains('paymentline')) {
+        node = node.parentNode
+      }
+      if (node) {
+        var currentOrder = node.line.pos.get('selectedOrder')
+        var paidTotal = currentOrder.getPaidTotal()
+        var dueTotal = currentOrder.getTotalTaxIncluded()
+        var remaining = dueTotal > paidTotal ? dueTotal - paidTotal : 0
 
-        click_set_change: function(event){
+        var newAmount = node.line.amount + remaining
+        node.querySelector('input').value = newAmount
+        node.line.set_amount(newAmount)
+      }
+      event.stopPropagation()
+    }
 
-            var node = this;
-            while(node && !node.classList.contains('paymentline')){
-                node = node.parentNode;
-            }
-            if(node){
-                var currentOrder = node.line.pos.get('selectedOrder');
-                var paidTotal = currentOrder.getPaidTotal();
-                var dueTotal = currentOrder.getTotalTaxIncluded();
-                var remaining = dueTotal > paidTotal ? dueTotal - paidTotal : 0;
+  })
 
-                var newAmount = node.line.amount + remaining;
-                node.querySelector('input').value = newAmount;
-                node.line.set_amount(newAmount);
-            }
-            event.stopPropagation();
-        },
-
-    });
-
-    /*************************************************************************
+  /*************************************************************************
         Extend ScreenSelector:
     */
-    var modulScreenSelectorParent = module.ScreenSelector;
-    module.ScreenSelector = module.ScreenSelector.extend({
+  var modulScreenSelectorParent = module.ScreenSelector
+  module.ScreenSelector = module.ScreenSelector.extend({
 
-        // do not switch to payment screen if the order is empty
-        // TODO (PORT V10/12) :: check if it is necessary.
-        // this double check (with addPaymentLine) is due to the bad
-        // design in Odoo Point of sale in the "click" event of the payment
-        // buttons that does'nt allow easy overload
-        set_current_screen: function(screen_name, params, refresh){
-            var order = this.pos.get('selectedOrder');
-            if (screen_name === 'payment' && order.get('orderLines').length === 0 && order.get('paymentLines').length === 0) {
-                return;
-            }
-            return modulScreenSelectorParent.prototype.set_current_screen.apply(this, arguments);
-        },
-    });
-
-
-};
+    // do not switch to payment screen if the order is empty
+    // TODO (PORT V10/12) :: check if it is necessary.
+    // this double check (with addPaymentLine) is due to the bad
+    // design in Odoo Point of sale in the "click" event of the payment
+    // buttons that does'nt allow easy overload
+    set_current_screen: function (screen_name, params, refresh) {
+      var order = this.pos.get('selectedOrder')
+      if (screen_name === 'payment' && order.get('orderLines').length === 0 && order.get('paymentLines').length === 0) {
+        return
+      }
+      return modulScreenSelectorParent.prototype.set_current_screen.apply(this, arguments)
+    }
+  })
+}
