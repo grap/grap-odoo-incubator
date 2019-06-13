ea8612b9d77e62a0ed712ecff6fc19e76232d127
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-06-13 17:48:13 +0200

    [MIG] 12.0 - pos_sector

--- pos_sector/__manifest__.py
+++ pos_sector/__manifest__.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # Copyright (C) 2014 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -21,7 +20,6 @@
         'views/view_pos_sector.xml',
         'views/view_product_template.xml',
         'views/view_pos_config.xml',
-        'views/action.xml',
         'views/menu.xml',
     ],
     'demo': [
--- pos_sector/models/__init__.py
+++ pos_sector/models/__init__.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 from . import pos_sector
 from . import pos_config
 from . import product_template
