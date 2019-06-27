b288c7d65021129ddfe3ca91272ef283ef88ff15
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-06-27 13:45:01 +0200

    [MIG] pos_check_session_state

--- pos_check_session_state/__manifest__.py
+++ pos_check_session_state/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2015-Today GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,7 +5,7 @@
 {
     'name': 'Point Of Sale - Check Session State',
     'summary': 'Check if the session state is still opened',
-    'version': '8.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Point Of Sale',
     'author': "GRAP",
     'website': 'http://www.grap.coop',
--- pos_check_session_state/static/src/js/pos_check_session_state.js
+++ pos_check_session_state/static/src/js/pos_check_session_state.js
@@ -4,10 +4,55 @@ Copyright (C) 2015-Today GRAP (http://www.grap.coop)
 License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
-"use strict";
+odoo.define('pos_check_session_state.pos_check_session_state', function (require){
 
-openerp.pos_check_session_state = function (instance) {
-    var module = instance.point_of_sale;
+    "use strict";
+    var PopupWidget = require('point_of_sale.popups');
+    var gui = require('point_of_sale.gui');
+    var chrome = require('point_of_sale.chrome');
+
+    var rpc = require('web.rpc');
+
+    /*
+        Overload build_widgets to add a check done every
+        check_session_state_frequency seconds, by a setInterval.
+    */
+    chrome.Chrome.include({
+
+        build_widgets: function () {
+            var self = this;
+            var res = this._super.apply(this, arguments);
+            var frequency = self.pos.config.check_session_state_frequency * 1000;
+
+            self.intervalIDCheckSessionState = setInterval(function() {
+                self._check_session_state();
+            }, frequency);
+            return res;
+        },
+
+        _check_session_state: function() {
+            var self = this;
+            var params = {
+                model: 'pos.session',
+                method: 'search_read',
+                domain: [['id', '=', self.pos.pos_session.id]],
+                fields: ['state'],
+            };
+
+            rpc.query(params)
+            .then(function(sessions){
+                if (sessions[0].state !== 'opened') {
+                    // warn user if current session is not opened
+                    self.gui.show_popup('error-closed-session', {session_state: sessions[0].state});
+                    clearInterval(self.intervalIDCheckSessionState);
+                }
+            })
+            .fail(function(error, event){
+                // Prevent error if server is unreachable
+                event.preventDefault();
+            });
+        }
+    });
 
     /*
         Define : New ErrorClosedSessionPopupWidget Widget.
@@ -15,49 +60,20 @@ openerp.pos_check_session_state = function (instance) {
         in an 'open' state;
         The check will be done depending on a parameter on the PoS config
     */
-    module.ErrorClosedSessionPopupWidget = module.ErrorPopupWidget.extend({
-        template:'ErrorClosedSessionPopupWidget',
+    var ErrorClosedSessionPopupWidget = PopupWidget.extend({
+        template: 'ErrorClosedSessionPopupWidget',
 
-        session_name: '',
-
-        init: function(parent, options) {
-            var self = this;
-            this._super(parent, options);
-
-            self.intervalID = setInterval(function() {
-                self.pos.fetch('pos.session', ['name', 'state'], [['id', '=', self.pos.pos_session.id]])
-                .then(function(sessions){
-                    if (sessions[0].state !== 'opened') {
-                        // warn user if current session is not opened
-                        self.session_name = sessions[0].name;
-                        self.renderElement();
-                        self.pos_widget.screen_selector.show_popup('error-closed-session');
-                        clearInterval(self.intervalID);
-                    }
-                })
-                .fail(function(error, event){
-                    // Prevent error if server is unreachable
-                    event.preventDefault();
-                });
-            }, self.pos.config.check_session_state_frequency * 1000);
+        show: function(options){
+            this._super(options);
+            this.gui.play_sound('error');
         },
+
     });
 
-    /*
-        Overload : PosWidget to include ErrorClosedSessionPopupWidget inside.
-    */
-    module.PosWidget = module.PosWidget.extend({
-        build_widgets: function(){
-            this._super();
-            // Add a new Popup 'ErrorClosedSessionPopupWidget'
-            this.error_closed_session_popup = new module.ErrorClosedSessionPopupWidget(this, {});
-            this.error_closed_session_popup.appendTo(this.$el);
-            this.screen_selector.popup_set['error-closed-session'] = this.error_closed_session_popup;
+    gui.define_popup({name:'error-closed-session', widget: ErrorClosedSessionPopupWidget});
 
-            // Hide the popup because all pop up are displayed at the
-            // beginning by default
-            this.error_closed_session_popup.hide();
-        },
-    });
+    return {
+        ErrorClosedSessionPopupWidget: ErrorClosedSessionPopupWidget,
+    };
 
-};
+});
