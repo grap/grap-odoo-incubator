1f5f05b59347eb955feb594e514ed22cc3f5e345
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-07-03 16:28:42 +0200

    [MIG] pos_street_market

--- pos_street_market/models/__init__.py
+++ pos_street_market/models/__init__.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 from . import market_place
 from . import pos_config
 from . import pos_order
--- pos_street_market/models/market_place.py
+++ pos_street_market/models/market_place.py
@@ -1,9 +1,8 @@
-# coding: utf-8
 # Copyright (C) 2015 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class MarketPlace(models.Model):
