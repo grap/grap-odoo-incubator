68c04a51ad2dcc0db003f2d864c7db879430e0d9
Quentin Dupont <perso@quentindupont.fr>
2019-07-17 12:04:42 +0200

    [12.0][MIG] Base legal info

--- base_company_legal_info/__manifest__.py
+++ base_company_legal_info/__manifest__.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base - Company Legal Information',
-    'version': '8.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Tools',
     'license': 'AGPL-3',
     'summary': 'Adds Legal informations on company model',
-    'author': 'Akretion,GRAP',
+    'author': 'Akretion,GRAP,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
     'depends': [
-        'report',
+        'web',
     ],
     'data': [
         'views/view_res_company.xml',
@@ -22,5 +21,5 @@
         'demo/res_groups.xml',
         'demo/res_company.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
