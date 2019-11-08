736dbaeffa0dc066a14cc7120c52243ee4504c22
Quentin Dupont <perso@quentindupont.fr>
2019-11-08 10:38:24 +0100

    [MIG] 8 → 12 [ADD] 1 test

--- stock_inventory_valuation/models/stock_inventory.py
+++ stock_inventory_valuation/models/stock_inventory.py
@@ -1,23 +1,25 @@
-# coding: utf-8
 # Copyright (C) 2013 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
-import openerp.addons.decimal_precision as dp
+from odoo import api, fields, models
+import odoo.addons.decimal_precision as dp
 
 
 class StockInventory(models.Model):
-    _inherit = 'stock.inventory'
+    _inherit = "stock.inventory"
 
     # Columns section
     valuation = fields.Float(
-        string='Valuation', compute='_compute_valuation', store=True,
-        digits_compute=dp.get_precision('Product Price'))
+        string="Valuation",
+        compute="_compute_valuation",
+        store=True,
+        digits=dp.get_precision("Product Price"),
+    )
 
     # Compute Section
     @api.multi
-    @api.depends('line_ids.valuation')
+    @api.depends("line_ids.valuation")
     def _compute_valuation(self):
         for inventory in self:
-            inventory.valuation = sum(inventory.mapped('line_ids.valuation'))
+            inventory.valuation = sum(inventory.mapped("line_ids.valuation"))
