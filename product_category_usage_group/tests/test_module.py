89d74e8cf0cca0eff0a073d48db36775b6bd8c51
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-13 21:28:54 +0100

    fixup! [MIG] product_category_usage_group/: Migration to 12.0

--- product_category_usage_group/tests/test_module.py
+++ product_category_usage_group/tests/test_module.py
@@ -11,22 +11,21 @@ class Tests(TransactionCase):
 
     def setUp(self):
         super(Tests, self).setUp()
-        self.template_obj = self.env["product.template"]
+        self.ProductTemplate = self.env["product.template"]
         self.user_demo = self.env.ref("base.user_demo")
-        self.user_root = self.env.ref("base.user_root")
+        self.user_admin = self.env.ref("base.user_admin")
         self.category_normal = self.env.ref("product.product_category_all")
         self.category_special = self.env.ref(
             "product_category_usage_group.category_usage_group"
         )
-        self.product_group = self.env.ref("sales_team.group_sale_manager")
-        self.restricted_group = self.env.ref("base.group_no_one")
+        self.product_group = self.env.ref("base.group_system")
+        self.restricted_group = self.env.ref("base.group_multi_company")
         # Set only Root user as member of the restricted group
         self.restricted_group.users = False
-        self.restricted_group.users = [self.user_root.id]
+        self.restricted_group.users = [self.user_admin.id]
         # Allow demo user to create template
         self.product_group.users = [self.user_demo.id]
 
-    # Test Section
     def test_01_access_user_without_right(self):
         # Use a category without restricted access should success
         self._create_product(self.user_demo, self.category_normal)
@@ -36,11 +35,11 @@ class Tests(TransactionCase):
 
     def test_02_access_user_with_right(self):
         # Use a category without restricted access should success
-        self._create_product(self.user_root, self.category_normal)
+        self._create_product(self.user_admin, self.category_normal)
         # Use a category with restricted access should success
-        self._create_product(self.user_root, self.category_special)
+        self._create_product(self.user_admin, self.category_special)
 
     def _create_product(self, user, category):
-        self.template_obj.sudo(user).create(
+        self.ProductTemplate.sudo(user).create(
             {"name": "Demo Product", "categ_id": category.id}
         )
