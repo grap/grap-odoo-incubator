396c39d5fe8ab0fe7209c4d1605b73d1653608dc
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-14 12:19:34 +0100

    [REF] account_fiscal_position_tax_included: Black python code

--- account_fiscal_position_tax_included/__openerp__.py
+++ account_fiscal_position_tax_included/__openerp__.py
@@ -4,25 +4,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Fiscal Position - Tax Excluded to Included',
-    'summary': 'Allow to map from tax excluded to tax included',
-    'version': '8.0.1.0.0',
-    'category': 'Accounting',
-    'author': 'GRAP',
-    'website': 'http://www.grap.coop',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
+    "name": "Account Fiscal Position - Tax Excluded to Included",
+    "summary": "Allow to map from tax excluded to tax included",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["views/view_account_fiscal_position.xml"],
+    "demo": [
+        "demo/res_groups.xml",
+        "demo/account_tax.xml",
+        "demo/account_fiscal_position.xml",
+        "demo/product_product.xml",
     ],
-    'data': [
-        'views/view_account_fiscal_position.xml',
-    ],
-    'demo': [
-        'demo/res_groups.xml',
-        'demo/account_tax.xml',
-        'demo/account_fiscal_position.xml',
-        'demo/product_product.xml',
-
-    ],
-    'installable': False,
+    "installable": False,
 }
--- account_fiscal_position_tax_included/models/account_tax.py
+++ account_fiscal_position_tax_included/models/account_tax.py
@@ -7,20 +7,22 @@ from openerp import models, api
 
 
 class AccountTax(models.Model):
-    _inherit = 'account.tax'
+    _inherit = "account.tax"
 
     @api.model
     def _fix_tax_included_price(self, price, prod_taxes, line_taxes):
         """Subtract tax amount from price when corresponding "price included"
         taxes do not apply. (for tax excluded)"""
         res = super(AccountTax, self)._fix_tax_included_price(
-            price, prod_taxes, line_taxes)
+            price, prod_taxes, line_taxes
+        )
 
         if price != res:
             return res
         dest_taxes = self.browse(line_taxes)
-        if any([x.price_include for x in dest_taxes])\
-                and not any([x.price_include for x in prod_taxes]):
+        if any([x.price_include for x in dest_taxes]) and not any(
+            [x.price_include for x in prod_taxes]
+        ):
             # it's a switch between vat exc and vat incl
             return price * (1 + dest_taxes[0].amount)
         return res
