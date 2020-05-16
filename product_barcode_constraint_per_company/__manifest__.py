655ebb7fb1049a019f025271041ce6a9c0bb1c93
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-05-16 22:38:44 +0200

    [ADD] product_barcode_constraint_per_company

---
+++ product_barcode_constraint_per_company/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2013-Today: GRAP (http://www.grap.coop)
+# @author: Julien WESTE
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product Barcode Constraint per Company",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "summary": "Change the product barcode constraint"
+    ", allowing the same barcode for differents companies",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "demo": [
+        "demo/res_company.xml",
+        "demo/product_product.xml",
+    ],
+    "installable": True,
+}
---
+++ product_barcode_constraint_per_company/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module extend Odoo functionnalities changing the constraint of the barcode
+on the ``product.product`` model.
+
+With this module, two products can have the same barcode, if there don't belong
+to the same company.
