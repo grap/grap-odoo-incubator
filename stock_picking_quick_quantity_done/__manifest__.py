e55d917294ad281e42e54af0d8ed6b64606f7dda
Quentin Dupont <perso@quentindupont.fr>
2020-09-02 12:33:58 +0200

    [ADD] Stock Picking Quick Quantity done

---
+++ stock_picking_quick_quantity_done/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2020-Today: GRAP (http://www.grap.coop)
+# @author Quentin DUPONT <quentin.dupont@grap.coop>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)..
+{
+    "name": "Stock Picking Quick Quantity Done",
+    "version": "12.0.1.0.1",
+    "category": "Stock",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "GRAP, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale",
+        "stock",
+    ],
+    "data": [
+        'views/view_stock_picking.xml',
+    ],
+}
