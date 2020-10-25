b30ab402c359be0b555cb9454877aae853930a42
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-10-25 09:36:52 +0100

    [ADD] product_category_type

---
+++ product_category_type/models/product_template.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    # Columns Section
+    categ_id = fields.Many2one(
+        comodel_name='product.category',
+        domain="[('type', '=', 'normal')]")
