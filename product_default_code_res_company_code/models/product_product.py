6d6bc0384239a005377de3611125097b0c8520d1
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-10-29 11:01:09 +0100

    [ADD] product_default_code_res_company_code : new module to generate product default code, based on company sequence, prefix with company code. (code comes from odoo-addons-cae/product_fiscal_company in V8

---
+++ product_default_code_res_company_code/models/product_product.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2014-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, api
+
+_SUFFIX_LENGTH = 6
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    # Overload Section
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        res._set_default_code()
+        return res
+
+    # Custom Section
+    @api.multi
+    def _set_default_code(self):
+        IrSequence = self.env['ir.sequence']
+        for product in self:
+            product.default_code = IrSequence.get(
+                'product_product.default_code')
