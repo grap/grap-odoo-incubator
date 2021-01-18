2af6c9334bf36e8d51bb9a047729cbba999bbf61
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-01-18 12:30:11 +0100

    [FIX] product_default_code_res_company_code : fix tests with post_install

--- product_default_code_res_company_code/tests/test_module.py
+++ product_default_code_res_company_code/tests/test_module.py
@@ -3,8 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
+from odoo.tests import tagged
 
 
+@tagged('post_install', '-at_install')
 class TestModule(TransactionCase):
     """Tests for 'product_default_code_res_company_code' Module"""
 
