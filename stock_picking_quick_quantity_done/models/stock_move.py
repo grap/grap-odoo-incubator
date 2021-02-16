c835675a3d543bdc96e0a05335300b6368db10b6
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-16 12:09:58 +0100

    [WIP] first adaptation to pre-commit

--- stock_picking_quick_quantity_done/models/stock_move.py
+++ stock_picking_quick_quantity_done/models/stock_move.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
 # @author Quentin DUPONT (quentin.dupont@grap.coop)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
