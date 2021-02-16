81753d28a860c20139ec4321f1c955b7707d8141
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-16 12:09:58 +0100

    [WIP] first adaptation to pre-commit

--- product_simple_pricelist/models/product_product.py
+++ product_simple_pricelist/models/product_product.py
@@ -3,8 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import _, fields, models
-from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError
+
+from odoo.addons import decimal_precision as dp
 
 
 class ProductProduct(models.Model):
