0061a241365679846fc79a48f053852b229e1061
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-07-08 14:38:10 +0200

    [FIX] set to_print, when we set the print category to a product

--- product_print_category/models/product_product.py
+++ product_print_category/models/product_product.py
@@ -15,7 +15,7 @@ class ProductProduct(models.Model):
         default=lambda s: s._default_print_category_id(),
     )
 
-    to_print = fields.Boolean(string="To Print", store=True)
+    to_print = fields.Boolean(string="To Print")
 
     # Default Section
     def _default_print_category_id(self):
@@ -25,22 +25,19 @@ class ProductProduct(models.Model):
     def create(self, vals):
         if vals.get("print_category_id", False):
             vals["to_print"] = True
-        return super(ProductProduct, self).create(vals)
+        return super().create(vals)
 
     @api.multi
     def write(self, vals):
-        res = super(ProductProduct, self).write(vals)
+        res = super().write(vals)
         product_ids = []
         # Set 'To print' if we change one field choosen in print_category
-        for product in self:
-            if product.print_category_id:
-                if len(
-                    list(
-                        set(vals.keys())
-                        & set(product.print_category_id.field_ids.mapped("name"))
-                    )
-                ):
-                    product_ids.append(product.id)
+        for product in self.filtered(lambda x: x.print_category_id):
+            triggering_fields = product.print_category_id.field_ids.mapped("name") + [
+                "print_category_id"
+            ]
+            if len(list(set(vals.keys()) & set(triggering_fields))):
+                product_ids.append(product.id)
         products = self.browse(product_ids)
         super(ProductProduct, products).write({"to_print": True})
         return res
