ec42ee9436a4a190a12e33eb726dc877d178532c
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-04-13 11:41:39 +0200

    TEST CI failing

--- duplication_sale_order/__manifest__.py
+++ duplication_sale_order/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Sale Order - Duplication",
+    "name": "Sale Order - Duplication",     
     "summary": "Duplication Tools for Sale Orders with a given frequency",
     "version": "12.0.1.1.2",
     "category": "Sale",
