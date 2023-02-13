e7e25de1056b1a1a5b85e162f21e5f3fc30a24a8
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-02-13 15:14:33 +0100

    --fixup=HEAD

--- account_invoice_invoice2data_test/__manifest__.py
+++ account_invoice_invoice2data_test/__manifest__.py
@@ -3,21 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Account Invoice - Invoice2data import (GRAP)",
+    "name": "Account Invoice - Test for Invoice2data Import (GRAP)",
     "version": "12.0.1.0.1",
     "category": "Accounting",
     "author": "GRAP",
     "website": "https://github.com/grap/grap-odoo-incubator",
     "license": "AGPL-3",
-    "depends": ["account"],
-    "data": [
-        "views/view_account_invoice.xml",
-        "wizards/wizard_invoice2data_import.xml",
-    ],
-    "external_dependencies": {"python": ["invoice2data"]},
+    "depends": ["account_invoice_invoice2data", "l10n_generic_coa"],
     "demo": [
-        "demo/res_partner.xml",
-        "demo/product_product.xml",
+        "demo/account_invoice.xml",
     ],
     "installable": True,
 }
---
+++ account_invoice_invoice2data_test/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module is a technical module written to test the module ``account_invoice_invoice2data`` module.
+
+It is not intended to be installed in production.
