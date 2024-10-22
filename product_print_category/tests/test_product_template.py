61ae3f8cc51a5e42c74c362dc04895f792a94f44
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2024-10-22 11:33:42 +0200

    [IMP] product_print_category: Recompute instead of overriding the compute method
    Overriding methods is bad practice. By simply recomputing at the correct
    stage in the write method, we achieve effectively the same.
    
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- product_print_category/tests/test_product_template.py
+++ product_print_category/tests/test_product_template.py
@@ -9,12 +9,17 @@ class TestProductTemplate(SavepointCase):
     def test_product_variant_id(self):
         """Test that product_variant_id is correctly populated for cloned
         archived products.
+
+        This test is needed because we now store the product_variant_id computed
+        field, instead of recomputing it every time.
         """
         product = self.env["product.template"].create({"name": "Test"})
         product.active = False
         new_product = product.copy()
         new_variants = new_product.with_context(active_test=False).product_variant_ids
         self.assertFalse(new_product.active)
-        self.assertEqual(new_product.product_variant_id, new_variants[0])
+        # This one is weird. The default Odoo upstream behaviour is that this
+        # field is False when the template is inactive. We replicate that.
+        self.assertFalse(new_product.product_variant_id)
         new_product.active = True
         self.assertEqual(new_product.product_variant_id, new_variants[0])
