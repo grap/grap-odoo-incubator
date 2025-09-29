83b2bf03ed0b80c754a92133d39bcc445da3939d
jssuzanne <js.suzanne@gmail.com>
2025-09-29 17:12:09 +0200

    Updated get_limited_products_loading

--- pos_sector/tests/__init__.py
+++ pos_sector/tests/__init__.py
@@ -1,1 +1,2 @@
 from . import test_pos_session
+from . import test_pos_config
---
+++ pos_sector/tests/test_pos_config.py
@@ -0,0 +1,27 @@
+import odoo
+
+from odoo.addons.point_of_sale.tests.common import TestPointOfSaleCommon
+
+
+@odoo.tests.tagged("post_install", "-at_install")
+class TestPointOfSaleConfig(TestPointOfSaleCommon):
+
+    def test_get_pos_ui_product_product_by_params(self):
+        sector = self.env["pos.sector"].create(
+            {
+                "name": "Test",
+                "company_id": self.pos_config.company_id.id,
+            }
+        )
+
+        products = self.pos_config.get_limited_products_loading(['sector_id'])
+        product_id = products[0]["id"]
+        product = self.env["product.product"].browse(product_id)
+
+        product.write({"sector_id": sector.id})
+        products = self.pos_config.get_limited_products_loading(['sector_id'])
+        self.assertNotIn(product_id, [x["id"] for x in products])
+
+        self.pos_config.sector_ids += sector
+        products = self.pos_config.get_limited_products_loading(['sector_id'])
+        self.assertIn(product_id, [x["id"] for x in products])
