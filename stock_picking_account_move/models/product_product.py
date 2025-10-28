d3066e27dd8c8984de9197f69417b58ffa404e89
Quentin Dupont <quentin.dupont@grap.coop>
2025-10-28 10:43:07 +0100

    [FIX] stock_picking_account_move tests failing

--- stock_picking_account_move/models/product_product.py
+++ stock_picking_account_move/models/product_product.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import _, models
-from odoo.exceptions import Warning as UserError
+from odoo.exceptions import UserError
 
 
 class ProductProduct(models.Model):
