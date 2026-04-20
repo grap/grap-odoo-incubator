dd57e765c04330fe8b9d31959465b15d782d7255
Quentin Dupont <quentin.dupont@grap.coop>
2026-04-20 15:34:51 +0200

    [16.0][ADD] account_move_reversal_stock

---
+++ account_move_reversal_stock/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2026 - Today: GRAP (https://www.grap.coop)
+# @author: Quentin DUPONT
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Move Reversal Stock",
+    "summary": "Facilitates the link between customer and inventory management.",
+    "version": "16.0.1.0.0",
+    "category": "Account",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-incubator",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "stock",
+        # OCA
+        "stock_picking_invoice_link",
+        "web_notify",
+    ],
+    "data": [
+        "views/view_account_move.xml",
+    ],
+    "installable": True,
+}
